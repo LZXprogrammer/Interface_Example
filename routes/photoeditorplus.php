d134757a9ba0dd1ca829e6beb6d90ad7e29d1671
LZXprogrammer <lzxprogrammer@outlook.com>
2018-06-12 11:46:07 +0800

    去除不要的文件

--- routes/photoeditorplus.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-/***********************   PhotoEditorPlus   ****************************/
-
-$app->get('PhotoEditorPlus/getAdControl', 'PhotoEditorPlusController@getAdControl');
-$app->post('PhotoEditorPlus/getAdControl', 'PhotoEditorPlusController@getAdControl');
